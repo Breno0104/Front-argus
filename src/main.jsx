86abef6a4f945a727bfd4d68cf98916909ad8ef5
Breno do nasicmento <Breno.ds263@gmail.com>
2025-01-21 21:07:45 -0300

    feat: homepage

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import "./index.css";
 
+import HomePage from "./pages/HomePage";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
+        {/* Rota para "/" */}
+        <Route path="/" element={<HomePage />} />
+        {/* Rota para "/login" */}
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
